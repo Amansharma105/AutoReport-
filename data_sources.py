155dded7a263a8d83c415f6583161e6382c70315
Amansharma105 <sharmaaman87600@gmail.com>
2026-06-18 12:45:53 +0530

    Add JSON and SQLite data adapters

--- data_sources.py
+++ data_sources.py
@@ -1,1 +1,29 @@
+import json
+import sqlite3
 
+class DataSource:
+    def load(self):
+        raise NotImplementedError
+
+
+class JSONDataSource(DataSource):
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def load(self):
+        with open(self.file_path, "r") as file:
+            return json.load(file)
+
+
+class SQLiteDataSource(DataSource):
+    def __init__(self, db_path, query):
+        self.db_path = db_path
+        self.query = query
+
+    def load(self):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        cursor.execute(self.query)
+        data = cursor.fetchall()
+        conn.close()
+        return data
