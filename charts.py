12262e9f97fa9af545c06cd49693a14a73789ca2
Amansharma105 <sharmaaman87600@gmail.com>
2026-06-22 12:27:17 +0530

    Add chart generation module using Matplotlib

--- charts.py
+++ charts.py
@@ -1,1 +1,17 @@
+import matplotlib.pyplot as plt
 
+def bar_chart(df, x, y):
+    plt.bar(df[x], df[y])
+    plt.show()
+
+def line_chart(df, x, y):
+    plt.plot(df[x], df[y])
+    plt.show()
+
+def pie_chart(df, column):
+    df[column].value_counts().plot.pie()
+    plt.show()
+
+def scatter_chart(df, x, y):
+    plt.scatter(df[x], df[y])
+    plt.show()
