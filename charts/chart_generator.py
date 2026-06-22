33391565b4c6e3f80a623f0eebe8147767d61d8e
Amansharma105 <sharmaaman87600@gmail.com>
2026-06-22 12:31:37 +0530

    Add chart generation module using Matplotlib

--- charts/chart_generator.py
+++ charts/chart_generator.py
@@ -1,1 +1,15 @@
+import matplotlib.pyplot as plt
 
+def bar_chart(labels, values):
+    plt.bar(labels, values)
+    plt.xlabel("Department")
+    plt.ylabel("Count")
+    plt.title("Department Chart")
+    plt.savefig("charts/bar_chart.png")
+    plt.close()
+
+def pie_chart(labels, values):
+    plt.pie(values, labels=labels, autopct="%1.1f%%")
+    plt.title("Department Distribution")
+    plt.savefig("charts/pie_chart.png")
+    plt.close()
