dd6c13d790577641a0e45993596530c4f9de9516
Amansharma105 <sharmaaman87600@gmail.com>
2026-06-24 19:36:24 +0530

    Add scheduler integration using APScheduler

--- scheduler.py
+++ scheduler.py
@@ -1,1 +1,11 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
 
+scheduler = BlockingScheduler()
+
+def generate_report():
+    print("Generating scheduled report...")
+
+scheduler.add_job(generate_report, "interval", minutes=1)
+
+if __name__ == "__main__":
+    scheduler.start()
